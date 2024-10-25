612a746bc0bb2314712a4f4906fc8e14977e7b59
Muhammed Shamil <sshazz93@gmail.com>
2024-10-25 16:50:51 +0530

    rating based on order

--- internal/controllers/ratingManagement.go
+++ internal/controllers/ratingManagement.go
@@ -43,6 +43,31 @@ func AddRating(c *gin.Context) {
 		})
 		return
 	}
+	var oritem model.OrderItem
+	if tx := database.DB.Model(&model.OrderItem{}).Where("product_id = ? AND user_id=?", pid, userId).First(&oritem); tx.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "user have no order history on this product!",
+		})
+		return
+	}
+	var erating model.Rating
+	if tx := database.DB.Model(&model.Rating{}).Where("product_id = ? AND user_id=?", pid, userId).First(&erating); tx.Error == nil {
+		database.DB.Model(&model.Rating{}).Where("product_id = ? AND user_id=?", pid, userId).Update("rating", uint(rat))
+		product.RatingSum += float64(rat) - float64(erating.Rating)
+		product.AverageRating = math.Round((product.RatingSum/float64(product.RatingCount))*10) / 10
+		if tx := database.DB.Model(&model.Product{}).Where("id=?", pid).Updates(map[string]interface{}{"rating_sum": product.RatingSum, "rating_count": product.RatingCount, "average_rating": product.AverageRating}); tx.Error != nil {
+			database.DB.Model(&model.Rating{}).Where("user_id=? AND product_id=?", userId, pid).Delete(&model.Rating{})
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Updation failed!",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Rating added",
+		})
+		return
+	}
 	rating := model.Rating{
 		UserID:    uint(userId),
 		ProductID: product.ID,
